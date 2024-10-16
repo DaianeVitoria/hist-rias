0c03fc49a5f25eef82ce8f042ba20f4eb4513f44
DaianeVitoria <162488563+DaianeVitoria@users.noreply.github.com>
2024-10-16 16:41:55 -0300

    16102024

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,10 +4,10 @@ async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas / 1e9)
-    const pessoasNoMundo = (dados.total_pessoas_mundo / * 1e9)
+    const pessoasNoMundo = (dados.total_pessoas_mundo / 1e9)
     const horas = parseInt(dados.tempo_medio)
     const minutos = Math.round((dados.tempo_medio - horas) * 100)
-    const porcentagemConctada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixe
+    const porcentagemConectada = ((pessoasConectadas / pessoasNoMundo ) * 100).toFixe
 
     const paragrafo = document.createElement('p')
     paragrafo.classList.add('graficos-container__texto')
@@ -18,7 +18,4 @@ async function vizualizarInformacoesGlobais() {
 }
 
 vizualizarInformacoesGlobais()
-
-    
-
 
